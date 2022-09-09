33677aee9b20f44522958b33913fa58acff12dd6
Simon Egorov <rhinoRofl@gmail.com>
2022-09-09 17:38:54 +0700

    Create 'search_or_create_building' func

--- main.py
+++ main.py
@@ -55,23 +55,17 @@ def parse_urls_from_paginated(city_url, parsing_depth: int) -> list:  # TODO ins
     job_finished = False
     while not job_finished:
         url = city_url.substitute(page=page)
-        print(url)
-
         # html_source = requests.get(url, headers=HEADERS).text
 
         with open('html.html', 'r') as file:
             html_source = file.read()
-            # file.write(html_source)
-        # print(html_source)
         job_finished = True
-
 
         soup_page = BeautifulSoup(html_source, 'lxml')
         # soup_page = BeautifulSoup(html_source, 'html.parser')
         flats_articles = soup_page.find_all('article', class_='_93444fe79c--container--Povoi _93444fe79c--cont--OzgVc')
         for flat in flats_articles:
             raw_timestamp = flat.find('div', class_='_93444fe79c--absolute--yut0v').text
-            # print(f'raw timestamp = {raw_timestamp}')
             raw_timestamp = raw_timestamp.split()
             offer_pub_datetime = datetime.now()
             if len(raw_timestamp) > 2:
@@ -84,10 +78,7 @@ def parse_urls_from_paginated(city_url, parsing_depth: int) -> list:  # TODO ins
             hours = int(hours_minutes[0])
             minutes = int(hours_minutes[1])
             offer_pub_datetime = offer_pub_datetime.replace(hour=hours, minute=minutes)
-            # print(f'offer_pub_datetime = {offer_pub_datetime}')
-            # print(f'parsing_depth = {parsing_depth}')
-            # print(f'calculation_result = {offer_pub_datetime - parsing_depth}')
-            # print('')
+
             if offer_pub_datetime - parsing_depth < timedelta(0):
                 job_finished = True
                 break
@@ -95,11 +86,83 @@ def parse_urls_from_paginated(city_url, parsing_depth: int) -> list:  # TODO ins
             result.append({link: offer_pub_datetime})
         page += 1
         # print('*** GO TO NEXT PAGE ***')
-        time.sleep(5)
+        time.sleep(3)
     return result
 
 
-def search_or_create_building(address: str) -> None:
+def parse_card_info(url_timestamp) -> dict:
+    card_info = {}
+    url = list(url_timestamp.keys())[0]
+    card_info['offer_datetime'] = url_timestamp.get(url)
+
+    print('***   START CARD SCRAPING   ***')
+    html_source = requests.get(url, headers=HEADERS).text
+    soup_page = BeautifulSoup(html_source, 'lxml')
+    # soup_page = BeautifulSoup(html_source, 'html.parser')
+    card_info['parse_datetime'] = datetime.now()
+
+    card_info['offer_id'] = int(url.split('/')[-2])
+    card_info['category'] = offer_types.get(soup_page.find('span', class_='a10a3f92e9--value--Y34zN').text)
+
+    price = soup_page.find('span', class_='a10a3f92e9--price_value--lqIK0').text
+    card_info['price'] = int(price[:-1].replace('\xa0', ''))
+
+    total_area = soup_page.find('div', class_='a10a3f92e9--info-value--bm3DC').text
+    card_info['total_area'] = float(total_area.split()[0])
+
+    floor_num = soup_page.find(
+        'div', class_='a10a3f92e9--info-value--bm3DC',
+        text=re.compile('из *')).text
+    card_info['floors_count'] = int(floor_num.split()[-1])
+    card_info['floor_num'] = int(floor_num.split()[0])
+
+    address_list = soup_page.find('address', class_='a10a3f92e9--address--F06X3').findChildren()
+    card_info['city'] = address_list[1].text
+    card_info['street'] = address_list[3].text[:-4]
+    card_info['house'] = address_list[4].text
+
+    building_about = soup_page.find_all('div', class_='a10a3f92e9--value--G2JlN')
+    card_info['year_house'] = int(building_about[0].text)
+    card_info['house_material_type'] = building_about[1].text
+
+    coordinates_script = soup_page.find('script', type='text/javascript', text=re.compile('"coordinates":*')).string
+    card_info['lat'] = float(re.search('"lat":(\d+)(\.)(\d+)', coordinates_script).group(0).split(':')[-1])
+    card_info['lon'] = float(re.search('"lng":(\d+)(\.)(\d+)', coordinates_script).group(0).split(':')[-1])
+
+    print('***   CARD SCRAPING IS DONE   ***')
+    return card_info
+
+
+def search_or_create_building(object_data: dict, connection) -> int:
+    address = f'Россия, {card_info.get("city")}, улица {card_info.get("street")}, {card_info.get("house")}'
+    search_building = f'''
+    SELECT building.building_id
+    FROM building
+    WHERE building.address = "{address}";
+    '''
+    object_entry = db.read_query(connection, query=search_building)
+    if len(object_entry):
+        print('***   BUILDING ENTRY DOES EXIST   ***')
+        building_id = int(object_entry[0][0])
+    else:
+        print('***   CREATING BUILDING ENTRY   ***')
+
+        city = object_data.get('city')
+        lat = object_data.get('lat')
+        lon = object_data.get('lon')
+        year_house = object_data.get('year_house')
+        floors_count = object_data.get('floors_count')
+        house_material_type = object_data.get('house_material_type')
+
+        create_building = f'''
+        INSERT INTO building (location, lat, lon, address, year_house, floors_count, house_material_type) VALUES
+        ("{city}", {lat}, {lon}, "{address}", {year_house}, {floors_count}, "{house_material_type}");
+        '''
+        db.execute_query(connection, query=create_building)
+        building_id = int(connection.insert_id())
+    return building_id
+
+
     # query = f'''
     # SELECT offer.offer_id
     # FROM offer
@@ -118,59 +181,39 @@ def search_or_create_building(address: str) -> None:
     pass
 
 
-def create_offer_entry(url: str):
-    search_or_create_building('типа адрес в моём формате')
+def create_or_update_offer_entry(object_data: dict, connection):
+    pprint(object_data)
+    building_id = search_or_create_building(object_data, connection)
+    query = f'''
+    SELECT offer.offer_id
+    FROM offer
+    WHERE offer_id = {object_data.get('offer_id')};
+    '''
+    object_entry = db.read_query(connection, query=query)
+    if len(object_entry):
+        print('***   CREATING OFFER ENTRY   ***')
+        create_offer = f'''
+         INSERT INTO offer (offer_id, category, price, total_area, floor_num, offer_datetime, building_id) VALUES
+        ({object_data.get("offer_id")}, {object_data.get("category")}, {object_data.get("price")},
+         {object_data.get("total_area")}, {object_data.get("floor_num")}, {object_data.get("offer_datetime")}, 2);
+    '''
+        # update entry only
+    else:
+        print('***   UPDATING OFFER ENTRY   ***')
+        # get_or_create building and create offer entry
+
+    # search_or_create_building('типа адрес в моём формате')
     'creating db entry'
-
-
-def parse_card_to_db(url_timestamp, connection):
-    card_info = {}
-    url = list(url_timestamp.keys())[0]
-    card_info['offer_datetime'] = url_timestamp.get(url)
-
-    html_source = requests.get(url, headers=HEADERS).text
-    soup_page = BeautifulSoup(html_source, 'lxml')
-    # soup_page = BeautifulSoup(html_source, 'html.parser')
-    card_info['parse_datetime'] = datetime.now()
-    print(url)
-
-    card_info['offer_id'] = int(url.split('/')[-2])
-    card_info['category'] = offer_types.get(soup_page.find('span', class_='a10a3f92e9--value--Y34zN').text)
-
-    price = soup_page.find('span', class_='a10a3f92e9--price_value--lqIK0').text
-    card_info['price'] = price[:-1].strip(' ')
-
-    total_area = soup_page.find('div', class_='a10a3f92e9--info-value--bm3DC').text
-    card_info['total_area'] = float(total_area.split()[0])
-
-    floor_num = soup_page.find(
-        'div', class_='a10a3f92e9--info-value--bm3DC',
-        text=re.compile('из *')).text
-    card_info['floors_count'] = int(floor_num.split()[-1])
-    card_info['floor_num'] = int(floor_num.split()[0])
-
-    address_list = soup_page.find('address', class_='a10a3f92e9--address--F06X3').findChildren()
-    card_info['city'] = address_list[1].text
-    card_info['street'] = address_list[3].text[:-4]
-    card_info['house'] = address_list[4].text
-    # address = f'Россия, {card_info.get("city")}, улица {card_info.get("street")}, {card_info.get("house")}'  # (Россия, Новосибирск, улица Дуси Ковальчук, 238)
-    location = card_info.get('city')
-
-    building_about = soup_page.find_all('div', class_='a10a3f92e9--value--G2JlN')
-    card_info['year_house'] = int(building_about[0].text)
-    card_info['house_material_type'] = building_about[1].text
-
-    coordinates_script = soup_page.find('script', type='text/javascript', text=re.compile('"coordinates":*')).string
-    card_info['lat'] = float(re.search('"lat":(\d+)(\.)(\d+)', coordinates_script).group(0).split(':')[-1])
-    card_info['lon'] = float(re.search('"lng":(\d+)(\.)(\d+)', coordinates_script).group(0).split(':')[-1])
 
 
 if __name__ == '__main__':
     flats_links = parse_urls_from_paginated(URL_NSK, parsing_depth=1)
     connection = db.create_db_connection(db.HOST_NAME, db.USERNAME,
                                          db.PASSWORD, db.DB_NAME)
-    # connection = 'DEBUG STRING'
-    parse_card_to_db(flats_links[0], connection)
+    card_info = parse_card_info(flats_links[0])
+    create_or_update_offer_entry(card_info, connection)
+
     # for flat in flats_links:
     #     parse_card_to_db(flat, connection)
     #     sleep(3)
+
